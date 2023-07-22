26b2c423f6639d6316b4d0984880c5e94456fb47
thirstybloodelf <84261270+thirstybloodelf@users.noreply.github.com>
2023-07-22 10:16:44 +0900

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    aci = {
+      source = "CiscoDevNet/aci"
+      //version = ">= 0.13"
+    }
+  
+  }
+}
+
+provider "aci" {
+    # cisco-aci user name
+    username = var.aci_username
+    # cisco-aci password
+    password = var.aci_password
+    # cisco-aci url
+    url      = var.aci_url
+    insecure = true
+}
+
